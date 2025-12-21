62f4f862bdbeb91c28786dccd5632215f6bd4f31
Krist Veseli <veselikr@msu.edu>
2025-12-20 21:29:57 -0500

    Implement Two Sum with O(n) hash map

---
+++ TwoSum.py
@@ -0,0 +1,31 @@
+# LeetCode Question #1
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    seen = {} # Mapping value's to indexes
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], index]
+        seen[num] = index
+
+if __name__ == "__main__":
+    # Test Case 1
+    nums = [2, 7, 11, 15]
+    target = 9
+    print("Test 1 Output:", twoSum(nums, target))  # Expected: [0, 1]
+
+    # Test Case 2
+    nums = [3, 2, 4]
+    target = 6
+    print("Test 2 Output:", twoSum(nums, target))  # Expected: [1, 2]
+
+    # Test Case 3
+    nums = [3, 3]
+    target = 6
+    print("Test 3 Output:", twoSum(nums, target))  # Expected: [0, 1]
