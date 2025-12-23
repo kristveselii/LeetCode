b308deea57ae87f00fbfbd8eaeed8cc8f52af0ae
Krist Veseli <veselikr@msu.edu>
2025-12-23 17:12:58 -0500

    O(nlogn) solution for Valid Anagram

---
+++ ValidAnagram.py
@@ -0,0 +1,22 @@
+# Leetcode Question #242
+
+def isAnagram(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    
+    # If the sorted versions of s & t are equal, they are anagrams
+    return sorted(s) == sorted(t)
+
+
+
+if __name__ == "__main__":
+    s = "anagram"
+    t = "nagaram"
+    print(isAnagram(s,t)) # Expected: True
+
+    s = "rat"
+    t = "car"
+    print(isAnagram(s, t)) # Expected: False
